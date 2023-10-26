f415f81b3e025ae0c8ecec17e7d68842bc83521a
fivaz <s.a.fivaz@gmail.com>
2023-10-26 09:33:47 +0200

    b

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,10 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/core/providers/github"
+import { PrismaAdapter } from '@auth/prisma-adapter';
 import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private"
+import prisma from '$lib/prisma';
 
 export const handle = SvelteKitAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
 })
